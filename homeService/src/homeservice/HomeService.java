8a9bc75b448848a58a79cfa6971ab9a8d41531d9
seym45 <seym45@gmail.com>
2016-11-12 22:26:36 +0600

    Academic Project on visual programming course
    This project is based on 'Home Service Solution'......

---
+++ homeService/src/homeservice/HomeService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homeservice;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author MUKIT
+ */
+public class HomeService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        signUp sign_Up=new signUp();
+//        sign_Up.setVisible(true);
+      homePage home_page=new homePage();
+       home_page.setVisible(true);
+//        employeeRegister emp = new employeeRegister();
+//        emp.setVisible(true);
+//        adminPanel admin_panel=new adminPanel();
+//        admin_panel.setVisible(true);
+    }
+    
+}
